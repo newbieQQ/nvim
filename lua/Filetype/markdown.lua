2063fb519d7694872d761d899988bf8329b39dfb
QQ <1770362456@qq.com>
2023-01-22 22:30:24 +0800

    update

--- lua/Filetype/markdown.lua
+++ lua/Filetype/markdown.lua
@@ -1,4 +1,4 @@
 local map = vim.api.nvim_set_keymap
-local openPreview = ":CocCommand markdown-preview-enhanced.openPreview<CR>:CocCommand markdown-preview-enhanced.openPreviewBackground<CR>"
+local openPreview = ":CocCommand markdown-preview-enhanced.openPreview<CR>"
 
 map('n', '<leader>r', openPreview, {noremap = false})
