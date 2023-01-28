2d33296bf71360fc73c93ebc6e19ae61c0ea4b95
QQ <1770362456@qq.com>
2023-01-28 18:52:00 +0800

    修复md关闭不能关闭预览

--- lua/Filetype/markdown.lua
+++ lua/Filetype/markdown.lua
@@ -4,7 +4,7 @@ G.cmd("au VimEnter * :MarkdownPreviewToggle")
 
 --set to 1, nvim will open the preview window after entering the markdown buffer
 --default: 0
-G.g.mkdp_auto_start = 0
+G.g.mkdp_auto_start = 1
 
 --set to 1, the nvim will auto close current preview window when change
 --from markdown buffer to another buffer
