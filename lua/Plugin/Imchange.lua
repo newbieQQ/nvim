63d15c011039fffaa0c843a6ff93a54b1f565a3e
QQ <1770362456@qq.com>
2023-01-28 18:50:00 +0800

    修复imchange

--- lua/Plugin/Imchange.lua
+++ lua/Plugin/Imchange.lua
@@ -3,14 +3,13 @@ local G, im = require("G"), "keyboard-us"
 function InsertLeave()
   local t = io.popen("fcitx5-remote -n")
   im = t:read()
-  G.cmd(":silent !fcitx5-remote -c,cr")
+  G.cmd(":silent !fcitx5-remote -c")
 end
 
 function InsertEnter()
   if im == 'pinyin'
   then
-    print(im)
-    G.cmd("!fcitx5-remote -s pinyin<cr>")
+    G.cmd(":silent !fcitx5-remote -s pinyin")
   end
 end
 
